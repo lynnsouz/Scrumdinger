90ea66f80a282e4c7dd867b00e3238ce541d51e0
Lynneker Souza <lnksouzacm@gmail.com>
2020-12-22 20:57:24 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lynneker Souza on 22/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
