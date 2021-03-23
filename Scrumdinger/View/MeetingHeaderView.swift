4df54f3e6f53ed59111c76517526eb2d7c1c0399
Lynneker Souza <lnksouzacm@gmail.com>
2021-03-22 21:56:21 -0300

    Update footer and header on MeetingView

---
+++ Scrumdinger/View/MeetingHeaderView.swift
@@ -0,0 +1,74 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Lynneker Souza on 21/03/21.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    private var progress: Double {
+        guard secondsRemaining > 0 else { return 1 }
+        let totalSeconds = Double(secondsElapsed + secondsRemaining)
+        return Double(secondsElapsed) / totalSeconds
+    }
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    private var minutesRemainingMetric: String {
+        minutesRemaining == 1 ? "minute" : "minutes"
+    }
+    let scrumColor: Color
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(scrumColor: scrumColor))
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    
+                    HStack {
+                        Text("\(secondsRemaining)")
+                        Image(systemName: "hourglass.tophalf.fill")
+                    }
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(Text("Time remaining"))
+        .accessibilityValue(Text("\(minutesRemaining) \(minutesRemainingMetric)"))
+        .padding([.top, .horizontal])
+    }
+}
+
+struct ScrumProgressViewStyle: ProgressViewStyle {
+    var scrumColor: Color
+
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(scrumColor.accessibleFontColor)
+                .frame(height: 20.0)
+            ProgressView(configuration)
+                .frame(height: 12.0)
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, scrumColor: DailyScrum.data[0].color)
+            .previewLayout(.sizeThatFits)
+    }
+}
